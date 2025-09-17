783a083c22833e74a3b89406a5f19e66cc34d802
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-09-17 18:51:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+let output = document.getElementById("output");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  let text = document.getElementById("text").value;
+  let delay = Number(document.getElementById("delay").value);
+  res(text, delay);
+});
+
+async function res(text,delay){
+	output.innerHTML = "";
+	try{
+		await new Promise((resolve)=>{
+			setTimeout(()=>{
+				resolve(text);
+			},delay);
+		})
+	   text.style.margin="10px";
+	   text.style.background_color="blue";
+	   output.innerHTML = text;
+	}
+	catch(err){
+		console.log(err);
+	}
+}
